6d767f1a38472ddbecf2395f053726ba2e71cf4f
AKERSKUUUG <AKERSKUUUG@17fe177e-0889-5ba2-13ee-6c875f88c948>
2012-01-27 10:29:50 +0000

    Fixed Timed 3G, now working.

--- trunk/ConnectivityTimer/src/com/connectivitymanager/core/Timed3GActivity.java
+++ trunk/ConnectivityTimer/src/com/connectivitymanager/core/Timed3GActivity.java
@@ -5,9 +5,7 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,6 +20,9 @@ import android.widget.Toast;
 
 import com.connectivitymanager.R;
 import com.connectivitymanager.alarm.DisconnectReceiver;
+import com.connectivitymanager.alarm.Timed3GReceiver;
+import com.connectivitymanager.utility.Constants;
+import com.connectivitymanager.utility.Tools;
 
 public class Timed3GActivity extends Activity {
 	private TextView tooltip;
@@ -31,20 +32,16 @@ public class Timed3GActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.timed_wifi);
+		setContentView(R.layout.timed_tg);
 
-		final Spinner durationSpinner =
-				(Spinner) findViewById(R.id.duration_input);
+		final Spinner durationSpinner = (Spinner) findViewById(R.id.tg_duration_input);
 		tooltip = (TextView) findViewById(R.id.timed_tg_tooltip);
 
 		am = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-		ArrayAdapter<CharSequence> adapter =
-				ArrayAdapter
-						.createFromResource(
-								this,
-								com.connectivitymanager.R.array.timed_wf_durations_array,
-								R.layout.my_simple_spinner_item);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, com.connectivitymanager.R.array.timed_wf_durations_array,
+				R.layout.my_simple_spinner_item);
 
 		durationSpinner.setAdapter(adapter);
 		// Default delay is 30 minutes
@@ -57,18 +54,16 @@ public class Timed3GActivity extends Activity {
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
 
-				String durationText =
-						durationSpinner.getSelectedItem().toString();
+				String durationText = durationSpinner.getSelectedItem()
+						.toString();
 				// Read the desired delay
-				durationHours =
-						Integer.parseInt(durationText.substring(0,
-								durationText.indexOf(':')));
+				durationHours = Integer.parseInt(durationText.substring(0,
+						durationText.indexOf(':')));
 
-				durationMinutes =
-						Integer.parseInt(durationText.substring(durationText
-								.indexOf(':') + 1));
+				durationMinutes = Integer.parseInt(durationText
+						.substring(durationText.indexOf(':') + 1));
 
-				// tooltip.setText(getString(R.string.timed_tg_tooltip_text));
+				tooltip.setText(getString(R.string.timed_tg_tooltip_text));
 			}
 
 			public void onNothingSelected(AdapterView<?> parent) {
@@ -77,33 +72,29 @@ public class Timed3GActivity extends Activity {
 
 		});
 
-		Button startButton = (Button) findViewById(R.id.timed_wf_startbutton);
+		Button startButton = (Button) findViewById(R.id.timed_tg_startbutton);
 
 		startButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				RadioButton radio =
-						(RadioButton) findViewById(R.id.connect_check);
+				RadioButton radio = (RadioButton) findViewById(R.id.tg_connect_check);
 				boolean checked = radio.isChecked();
 
 				Calendar cal = Calendar.getInstance();
 
-				cal.add(Calendar.HOUR, durationHours);
-				cal.add(Calendar.MINUTE, durationMinutes);
+				cal.add(Constants.DURATION * 60, durationHours);
+				cal.add(Constants.DURATION, durationMinutes);
 
-				Intent intent =
-						new Intent(Timed3GActivity.this,
-								DisconnectReceiver.class);
+				Intent intent = new Intent(Timed3GActivity.this,
+						Timed3GReceiver.class);
 
-				intent.putExtra("3g_enable", checked);
+				intent.putExtra("tg_enable", !checked);
 
-				WifiManager wfMgr =
-						(WifiManager) getSystemService(Context.WIFI_SERVICE);
-				wfMgr.setWifiEnabled(!checked);
+				Tools.set3GEnabled(Timed3GActivity.this, checked);
 
-				PendingIntent sender =
-						PendingIntent.getBroadcast(Timed3GActivity.this, 0,
-								intent, PendingIntent.FLAG_UPDATE_CURRENT);
+				PendingIntent sender = PendingIntent.getBroadcast(
+						Timed3GActivity.this, 0, intent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
 
 				Toast.makeText(getApplicationContext(),
 						getString(R.string.service_started), Toast.LENGTH_SHORT)
@@ -122,17 +113,16 @@ public class Timed3GActivity extends Activity {
 			}
 		});
 
-		Button stopButton = (Button) findViewById(R.id.timed_wf_stopbutton);
+		Button stopButton = (Button) findViewById(R.id.timed_tg_stopbutton);
 
 		stopButton.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				Intent tempIntent =
-						new Intent(Timed3GActivity.this,
-								DisconnectReceiver.class);
-				PendingIntent tempSender =
-						PendingIntent.getBroadcast(Timed3GActivity.this, 0,
-								tempIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+				Intent tempIntent = new Intent(Timed3GActivity.this,
+						DisconnectReceiver.class);
+				PendingIntent tempSender = PendingIntent.getBroadcast(
+						Timed3GActivity.this, 0, tempIntent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
 
 				am.cancel(tempSender);
 
