9b8479d3d660728d7eb6676c7993c4f8fe37c0f8
AKERSKUUUG <AKERSKUUUG@17fe177e-0889-5ba2-13ee-6c875f88c948>
2012-01-30 15:37:12 +0000

    Added working connection checkboxes to MainActivity

--- ConnectivityTimer/src/com/connectivitymanager/core/MainActivity.java
+++ ConnectivityTimer/src/com/connectivitymanager/core/MainActivity.java
@@ -1,15 +1,27 @@
 package com.connectivitymanager.core;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import com.connectivitymanager.R;
+import com.connectivitymanager.utility.Tools;
 
 public class MainActivity extends Activity {
+
+	private CheckBox check3G, checkWifi;
+	private WifiManager wfMgr;
+
+	public static int SDK_VERSION = 8;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +31,16 @@ public class MainActivity extends Activity {
 		Button wifiWatcherButton = (Button) findViewById(R.id.wfwatcher_button);
 		Button timedWifiButton = (Button) findViewById(R.id.timed_wf_button);
 		Button timed3GButton = (Button) findViewById(R.id.timed_tg_button);
-		if (Integer.valueOf(android.os.Build.VERSION.SDK) <= 8) {
+		check3G = (CheckBox) findViewById(R.id.main_threeg_enable_check);
+		checkWifi = (CheckBox) findViewById(R.id.main_wifi_enable_check);
+
+		wfMgr = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+		SDK_VERSION = Integer.valueOf(android.os.Build.VERSION.SDK);
+
+		if (SDK_VERSION <= 8) {
 			timed3GButton.setVisibility(View.GONE);
+			check3G.setVisibility(View.GONE);
 		}
 
 		wifiWatcherButton.setOnClickListener(new OnClickListener() {
@@ -49,5 +69,44 @@ public class MainActivity extends Activity {
 				MainActivity.this.startActivity(intent);
 			}
 		});
+
+		check3G.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				Tools.set3GEnabled(MainActivity.this, isChecked);
+
+			}
+		});
+
+		checkWifi.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+
+				wfMgr.setWifiEnabled(isChecked);
+
+			}
+		});
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		ConnectivityManager netInfo = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		android.net.NetworkInfo mobile = netInfo
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+		check3G = (CheckBox) findViewById(R.id.main_threeg_enable_check);
+		checkWifi = (CheckBox) findViewById(R.id.main_wifi_enable_check);
+
+		// Set the checkboxes' status according to the network status
+		if (SDK_VERSION > 8) {
+			check3G.setChecked(Tools.get3GDataEnabled(this));
+		}
+		checkWifi.setChecked(wfMgr.isWifiEnabled());
+
 	}
 }
