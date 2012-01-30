064f5938330b8c56f8dcaaca3e8980f60e38bcd8
akerskuuug@gmail.com <akerskuuug@gmail.com@17fe177e-0889-5ba2-13ee-6c875f88c948>
2012-01-30 13:36:37 +0000

    TODO: Fix toggle for 3G / Wifi and set Activities for notif

--- ConnectivityTimer/src/com/connectivitymanager/alarm/Timed3GReceiver.java
+++ ConnectivityTimer/src/com/connectivitymanager/alarm/Timed3GReceiver.java
@@ -1,9 +1,13 @@
 package com.connectivitymanager.alarm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.connectivitymanager.core.DisconnectTimerActivity;
 import com.connectivitymanager.utility.Tools;
 
 public class Timed3GReceiver extends BroadcastReceiver {
@@ -11,8 +15,46 @@ public class Timed3GReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
-		Tools.set3GEnabled(context,
-				intent.getExtras().getBoolean("tg_enable", false));
+		boolean setEnabled = intent.getExtras().getBoolean("tg_enable", false);
+		CharSequence tickerText = "";
+		String description = "";
+
+		Tools.set3GEnabled(context, setEnabled);
+
+		NotificationManager nm =
+				(NotificationManager) context
+						.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// Create the visuals for the notification
+		int icon = android.R.drawable.star_on;
+
+		if (setEnabled) {
+			tickerText =
+					context.getString(com.connectivitymanager.R.string.tg_enabled);
+			description =
+					context.getString(com.connectivitymanager.R.string.tg_enabled_set_time);
+		} else {
+
+			tickerText =
+					context.getString(com.connectivitymanager.R.string.tg_disabled);
+			description =
+					context.getString(com.connectivitymanager.R.string.tg_disabled_set_time);
+		}
+		long when = System.currentTimeMillis();
+		Notification notification = new Notification(icon, tickerText, when);
+
+		// Decide what will be displayed in the notification bar
+		// and what will happen when the notification is clicked
+		Intent notificationIntent =
+				new Intent(context, DisconnectTimerActivity.class);
+		PendingIntent contentIntent =
+				PendingIntent.getActivity(context, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, tickerText, description,
+				contentIntent);
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		// Show the notification
+		nm.notify(3217, notification);
 
 	}
 
--- ConnectivityTimer/src/com/connectivitymanager/alarm/TimedWifiReceiver.java
+++ ConnectivityTimer/src/com/connectivitymanager/alarm/TimedWifiReceiver.java
@@ -1,20 +1,63 @@
 package com.connectivitymanager.alarm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 
+import com.connectivitymanager.core.DisconnectTimerActivity;
+
 public class TimedWifiReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		WifiManager wfMgr = (WifiManager) context
-				.getSystemService(Context.WIFI_SERVICE);
+		WifiManager wfMgr =
+				(WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+		CharSequence tickerText = "";
+		String description = "";
 
 		boolean setWifiTo = intent.getExtras().getBoolean("wifi_enable", false);
 
 		wfMgr.setWifiEnabled(setWifiTo);
+
+		NotificationManager nm =
+				(NotificationManager) context
+						.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// Create the visuals for the notification
+		int icon = android.R.drawable.star_on;
+
+		if (setWifiTo) {
+			tickerText =
+					context.getString(com.connectivitymanager.R.string.wifi_enabled);
+			description =
+					context.getString(com.connectivitymanager.R.string.wifi_enabled_set_time);
+		} else {
+
+			tickerText =
+					context.getString(com.connectivitymanager.R.string.wifi_disabled);
+			description =
+					context.getString(com.connectivitymanager.R.string.wifi_disabled_set_time);
+		}
+		long when = System.currentTimeMillis();
+		Notification notification = new Notification(icon, tickerText, when);
+
+		// Decide what will be displayed in the notification bar
+		// and what will happen when the notification is clicked
+		Intent notificationIntent =
+				new Intent(context, DisconnectTimerActivity.class);
+		PendingIntent contentIntent =
+				PendingIntent.getActivity(context, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, tickerText, description,
+				contentIntent);
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		// Show the notification
+		nm.notify(3218, notification);
 
 	}
 
